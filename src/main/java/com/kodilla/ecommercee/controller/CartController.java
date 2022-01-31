6d0a33c4fae6c095d90e15d40d82dd8862559dcf
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-01-31 20:19:48 +0100

    CartController added

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,4 +1,28 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.CartDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/cart")
 public class CartController {
+
+    @GetMapping("/getCart/{id}")
+    public CartDto getCart(@PathVariable Long id) {
+        return new CartDto();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, value = "/createCart")
+    public void createCart(@RequestBody CartDto cartDto) {
+    }
+
+    @PutMapping(value = "/updateCart")
+    public CartDto updateCart(@RequestBody CartDto cartDto) {
+        return new CartDto();
+    }
+
+    @DeleteMapping(value = "/deleteCart/{id}")
+    public void deleteCart(@PathVariable Long id) {
+    }
 }
