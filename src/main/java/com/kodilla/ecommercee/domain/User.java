6223e5ec63904da4d318a3468da4915f1773e4e8
Piotr Kusibab <starszykruku@op.pl>
2022-01-31 14:41:38 +0100

    created entity user according to the guidelines agreed with the team

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,4 +1,30 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "USERS")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private Long id;
+    @NotNull
+    private String username;
+    private String email;
+    @NotNull
+    private String password;
+    private LocalDateTime createDate;
+    @NotNull
+    private boolean isActive;
+    @NotNull
+    private boolean isEnabled;
 }
