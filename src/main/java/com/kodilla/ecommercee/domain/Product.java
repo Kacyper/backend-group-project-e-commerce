92b2bd4fc90c1049c010d832e38b76c58cc197b9
Kacper <kksykiewicz@gmail.com>
2022-01-31 20:41:08 +0100

    Product entity added

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,4 +1,42 @@
 package com.kodilla.ecommercee.domain;
 
+
+
+import jdk.jfr.Unsigned;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+
+@Entity(name = "PRODUCTS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Product {
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Unsigned
+    @Column(name = "ID_PRODUCT", unique = true)
+    private Long id;
+
+    @NotNull
+    @Column(name = "NAME")
+    private String name;
+
+    @NotNull
+    @Column(name = "QUANTITY")
+    private int quantity;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID_GROUP")
+    private Group group;
+
 }
