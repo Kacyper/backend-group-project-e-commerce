ceb7263c4e60274e6620047b6b1207ec75ff59da
jakub <jakub.dudek94@gmail.com>
2022-01-31 16:44:53 +0100

    GroupController + GroupDto created

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -1,4 +1,37 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.GroupDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/v1/group")
 public class GroupController {
+
+    @GetMapping(value = "getGroups")
+    public List<GroupDto> getGroups() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "getGroup")
+    public GroupDto getGroup(@RequestParam Long groupId) throws Exception {
+        return new GroupDto();
+    }
+
+    @PostMapping(value = "createGroup")
+    public void createGroup(@RequestBody GroupDto groupDto) {
+
+    }
+
+    @PutMapping(value = "updateGroup")
+    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
+        return new GroupDto();
+    }
+
+    @DeleteMapping(value = "deleteGroup")
+    public void deleteGroup(@RequestParam Long groupId) {
+
+    }
 }
--- src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
@@ -1,4 +1,15 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GroupDto {
+    private Long id;
+    private String groupName;
+    private double price;
+    private String description;
 }
