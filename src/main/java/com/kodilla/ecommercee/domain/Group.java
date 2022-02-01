23e41a8e63971bf280e71f5bf7cd75da18efe1cd
kpikula <90643790+kpikula@users.noreply.github.com>
2022-02-01 21:50:59 +0100

    New Group entity created

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,4 +1,48 @@
 package com.kodilla.ecommercee.domain;
 
+import jdk.jfr.Unsigned;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+
+@Entity(name = "GROUPS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    @Unsigned
+    @Column(name = "ID_GROUP", unique = true)
+    private Long groupId;
+
+    @NotNull
+    @Column(name = "NAME")
+    private String groupName;
+
+    @NotNull
+    @Column(name = "PRICE")
+    private double price;
+
+    @Column(name = "product_description")
+    private String productDescription;
+
+
+    @OneToMany(
+            mappedBy = "group",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Product> products;
+    
 }
