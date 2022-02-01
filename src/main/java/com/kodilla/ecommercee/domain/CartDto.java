9c2fd3243760d223a6e313dd1ac969184b297b6a
Kacper <kksykiewicz@gmail.com>
2022-02-01 21:10:37 +0100

    Cart and CartDto added.

--- src/main/java/com/kodilla/ecommercee/domain/CartDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/CartDto.java
@@ -1,4 +1,17 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+
 public class CartDto {
+
+    private int idCart;
+    private String name;
+    private BigDecimal total;
+
 }
--- src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
@@ -1,4 +1,18 @@
 package com.kodilla.ecommercee.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+
 public class ProductDto {
+
+    private Long id;
+    private String name;
+    private int quantity;
+    private Long groupId;
+
 }
