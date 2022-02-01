01d90dddf67b8a2a349b868ccfdfd73ee055a938
Piotr Kusibab <starszykruku@op.pl>
2022-02-01 13:53:16 +0100

    i added fields to ProductDto to make tests with postman possible, i also updated Product controller to return dummy data(changed delete method to return id of deleted ProductDto

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -17,21 +17,36 @@ public class ProductController {
 
     @GetMapping("/{id}")
     public ProductDto getProduct(@PathVariable Long id) {
-        return new ProductDto();
+        return ProductDto.builder()
+                .id(1L)
+                .name("name")
+                .quantity(123)
+                .groupId(1L)
+                .build();
     }
 
     @PostMapping
     public ProductDto createProduct(@RequestBody ProductDto productDto) {
-        return new ProductDto();
+        return ProductDto.builder()
+                .id(productDto.getId())
+                .name(productDto.getName())
+                .quantity(productDto.getQuantity())
+                .groupId(productDto.getGroupId())
+                .build();
     }
 
-    @PutMapping
-    public ProductDto updateProduct(@RequestBody ProductDto productDto) {
-        return new ProductDto();
+    @PutMapping("/{id}")
+    public ProductDto updateProduct(@PathVariable Long id, @RequestBody ProductDto productDto) {
+        return ProductDto.builder()
+                .id(id)
+                .name(productDto.getName())
+                .quantity(productDto.getQuantity())
+                .groupId(productDto.getGroupId())
+                .build();
     }
 
     @DeleteMapping("/{id}")
-    public void deleteProduct(@PathVariable Long id) {
-
+    public Long deleteProduct(@PathVariable Long id) {
+        return id;
     }
 }
