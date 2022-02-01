3c4ac328a524aed5f519934bde74002f09e9891b
Basixx <barbara_rutkowska.22@wp.pl>
2022-02-01 22:57:02 +0100

    UserController and UserDto added

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,4 +1,39 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.UserDto;
+import org.springframework.web.bind.annotation.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+
+    @GetMapping
+    public List<UserDto> getUsers(){
+        return new ArrayList<>();
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody UserDto userDto){
+
+    }
+
+    @PutMapping
+    public UserDto blockUser (@RequestBody UserDto userDto){
+        return new UserDto(
+                1L,
+                "John1231",
+                "john@john.com",
+                "password1223",
+                LocalDate.of(2022, 1, 1),
+                false,
+                true);
+    }
+
+    @PostMapping(value = "generateKey")
+    public int generateKey(@RequestBody UserDto userDto){
+        return 1;
+    }
 }
