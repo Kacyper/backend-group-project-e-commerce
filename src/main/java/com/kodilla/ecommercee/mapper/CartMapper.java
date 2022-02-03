0bd33dac2a054c3e16dcec79a995c7e7d7fb59d6
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-03 20:03:54 +0100

    MapToCart method added to CartMapper

--- src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
@@ -1,4 +1,19 @@
 package com.kodilla.ecommercee.mapper;
 
+import com.kodilla.ecommercee.domain.Cart;
+import com.kodilla.ecommercee.domain.CartDto;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CartMapper {
+
+    public Cart mapToCart(final CartDto cartDto) {
+        return new Cart(
+                cartDto.getIdCart()/*,
+
+                Will be uncommented after full Cart implementation (JDP220102-14)
+                cartDto.getName(),
+                cartDto.getTotal()*/
+        );
+    }
 }
