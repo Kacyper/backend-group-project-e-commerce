e28a5f267b1b9b97b855c92e5f1114ed2fe59cd8
donzdanowicz <madpz89@gmail.com>
2022-02-03 21:49:07 +0100

    Added Order entity

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,4 +1,46 @@
 package com.kodilla.ecommercee.domain;
 
+
+import jdk.jfr.Unsigned;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity(name = "CARTS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Cart {
+
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Unsigned
+    @NotNull
+    @Column(name = "ID_CART")
+    private Long idCart;
+
+    @NotNull
+    @Column(name = "NAME")
+    private String name;
+
+    @NotNull
+    @Column(name = "TOTAL")
+    private BigDecimal total;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "ID_ORDER")
+    private Order order;
+
+
+
+
 }
--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -1,11 +1,14 @@
 package com.kodilla.ecommercee.domain;
 
+import jdk.jfr.Unsigned;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,8 +19,38 @@ import javax.persistence.*;
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    @Unsigned
+    @Column(name = "ID_ORDER", unique = true)
     private Long id;
+
+    @NotNull
+    @Column(name = "ORDER_DATE")
+    private LocalDate orderDate;
+
+    @NotNull
+    @Unsigned
+    @Column(name = "SHIPPING_PRICE")
+    private double shippingPrice;
+
+    @NotNull
+    @Unsigned
+    @Column(name = "TOTAL_PRICE")
+    private double totalPrice;
+
+    @NotNull
+    @Column(name = "IS_SENT")
+    private boolean isSent;
+
+    @NotNull
+    @Column(name = "IS_PAID")
+    private boolean isPaid;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "ID_CART")
+    private Cart cart;
+
     @ManyToOne
-    @JoinColumn(name = "USER_ID")
+    @JoinColumn(name = "ID_USER")
     private User user;
 }
