eb108d96e5323372b0bdea3bf17cf181a9a09c89
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-03 19:31:13 +0100

    CartNotFoundException and GlobalHttpErrorHandler created

---
+++ src/main/java/com/kodilla/ecommercee/controller/GlobalHttpErrorHandler.java
@@ -0,0 +1,16 @@
+package com.kodilla.ecommercee.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(CartNotFoundException.class)
+    public ResponseEntity<Object> handleCartNotFoundException(CartNotFoundException cartNotFoundException) {
+        return new ResponseEntity<>("Cart with given id doesn't exist or can't be found", HttpStatus.BAD_REQUEST);
+    }
+}
