b539178dcba04692512abe629463ad331d7ab787
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-03 19:57:09 +0100

    CartRepository extended with CrudRepository

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,4 +1,28 @@
 package com.kodilla.ecommercee.domain;
 
+import com.sun.istack.NotNull;
+import jdk.jfr.Unsigned;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "CARTS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cart {
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Unsigned
+    @Column(name = "ID_CART")
+    private Long idCart;
 }
--- src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -1,4 +1,9 @@
 package com.kodilla.ecommercee.repository;
 
-public interface CartRepository {
+import com.kodilla.ecommercee.domain.Cart;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends CrudRepository<Cart, Long> {
 }
