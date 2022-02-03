8224a8315d8d124b7b1e55bb0cdc274b4bbb07ff
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-03 19:59:57 +0100

    saveCart() method added to DbServiceCart

--- src/main/java/com/kodilla/ecommercee/service/DbServiceCart.java
+++ src/main/java/com/kodilla/ecommercee/service/DbServiceCart.java
@@ -1,4 +1,17 @@
 package com.kodilla.ecommercee.service;
 
+import com.kodilla.ecommercee.domain.Cart;
+import com.kodilla.ecommercee.repository.CartRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class DbServiceCart {
+
+    private final CartRepository cartRepository;
+
+    public Cart saveCart(final Cart cart) {
+        return cartRepository.save(cart);
+    }
 }
