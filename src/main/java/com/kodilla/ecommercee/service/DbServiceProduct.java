d194e0d8f706f394614e5c3b84a10b17fe275a62
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-04 14:14:47 +0100

    ProductNotFoundException created and DbServiceProduct debugged

--- src/main/java/com/kodilla/ecommercee/service/DbServiceProduct.java
+++ src/main/java/com/kodilla/ecommercee/service/DbServiceProduct.java
@@ -1,8 +1,13 @@
 package com.kodilla.ecommercee.service;
 
+import com.kodilla.ecommercee.controller.ProductNotFoundException;
 import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class DbServiceProduct {
 
     private final ProductRepository productRepository;
