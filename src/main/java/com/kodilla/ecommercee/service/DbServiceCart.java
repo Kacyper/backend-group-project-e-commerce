76455b6d265bd3448607dc5c3af355084dd28884
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-04 15:10:24 +0100

    updateCart() added to DbServiceCart

--- src/main/java/com/kodilla/ecommercee/service/DbServiceCart.java
+++ src/main/java/com/kodilla/ecommercee/service/DbServiceCart.java
@@ -3,9 +3,11 @@ package com.kodilla.ecommercee.service;
 import com.kodilla.ecommercee.domain.Cart;
 import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.repository.CartRepository;
+import com.kodilla.ecommercee.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +16,8 @@ import java.util.Optional;
 public class DbServiceCart {
 
     private final CartRepository cartRepository;
+    private final DbServiceProduct dbServiceProduct;
+    private final ProductRepository productRepository;
 
     public Cart saveCart(final Cart cart) {
         return cartRepository.save(cart);
@@ -28,6 +32,21 @@ public class DbServiceCart {
         return products;
     }
 
-    public void updateCart(final Long idCart, final Long idProduct) {
+    public Optional<Cart> updateCart(final Long idCart, final Long idProduct) {
+        Optional<Cart> cart = cartRepository.findById(idCart);
+        BigDecimal currentTotalFromCart = cart.get().getTotal();
+        BigDecimal productPrice = new BigDecimal(0);
+
+        if (dbServiceProduct.ifExist(idProduct)) {
+            Optional<Product> product = productRepository.findById(idProduct);
+            productPrice = product.get().getPrice();
+
+        } else {
+            System.out.println("Product with id: " + idProduct + " doesn't exist or can't be found");
+        }
+
+        BigDecimal newCurrentTotalFromCart = currentTotalFromCart.add(productPrice);
+        cart.get().setTotal(newCurrentTotalFromCart);
+        return cart;
     }
 }
