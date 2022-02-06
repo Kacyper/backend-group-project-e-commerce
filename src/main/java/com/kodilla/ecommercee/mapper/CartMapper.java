b724ecd6bbe404e58bf1adbf6dacb7b08ae8acae
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-06 11:08:14 +0100

    mapToProductsDto() added to CartMapper

--- src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
@@ -1,13 +1,14 @@
 package com.kodilla.ecommercee.mapper;
 
-import com.kodilla.ecommercee.domain.Cart;
-import com.kodilla.ecommercee.domain.CartDto;
-import com.kodilla.ecommercee.domain.Order;
+import com.kodilla.ecommercee.domain.*;
 import org.springframework.stereotype.Service;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class CartMapper {
+
+    private ProductMapper productMapper;
 
     public Cart mapToCart(final CartDto cartDto) {
         return new Cart(
@@ -25,5 +26,11 @@ public class CartMapper {
                 cart.getCartName(),
                 cart.getTotal()
         );
+    }
+
+    public List<ProductDto> mapToProductsDto(final List<Product> products) {
+        return products.stream()
+                .map(product -> productMapper.mapToProductDto(product))
+                .collect(Collectors.toList());
     }
 }
