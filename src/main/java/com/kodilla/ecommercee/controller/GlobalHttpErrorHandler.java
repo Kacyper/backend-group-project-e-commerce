355a5b9765e2efbbe799847e16f4b2023747888e
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-06 21:33:33 +0100

    handleProductNotFoundInCartException() added to GlobalHttpErrorException

--- src/main/java/com/kodilla/ecommercee/controller/GlobalHttpErrorHandler.java
+++ src/main/java/com/kodilla/ecommercee/controller/GlobalHttpErrorHandler.java
@@ -18,4 +18,10 @@ public class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleProductNotFoundException(ProductNotFoundException productNotFoundException) {
         return new ResponseEntity<>("Product with given id doesn't exist or can't be found", HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<Object> handleProductNotFoundInCartException(ProductNotFoundInCartException productNotFoundInCartException) {
+        return new ResponseEntity<>("Product with given id not found in the given cart", HttpStatus.NOT_FOUND);
+    }
+
 }
