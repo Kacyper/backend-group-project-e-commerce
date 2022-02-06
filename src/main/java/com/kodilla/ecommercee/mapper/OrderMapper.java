8618e948b8bd68ecd5947594d83a825a770d615c
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-06 14:01:24 +0100

    mapToOrderDto() added to OrderMapper

--- src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
@@ -1,4 +1,16 @@
 package com.kodilla.ecommercee.mapper;
 
+import com.kodilla.ecommercee.domain.Order;
+import com.kodilla.ecommercee.domain.OrderDto;
+
 public class OrderMapper {
+    public OrderDto mapToOrderDto(final Order order) {
+        return OrderDto.builder()
+                .orderDate(order.getOrderDate())
+                .shippingPrice(order.getShippingPrice().doubleValue())
+                .totalPrice(order.getTotalPrice().doubleValue())
+                .isSent(order.isSent())
+                .isPaid(order.isPaid())
+                .build();
+    }
 }
