0cf068a1738a0c36cf428cfb997bdce8f867ab94
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-14 23:49:39 +0100

    CartMapper refactored

--- src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
@@ -2,23 +2,13 @@ package com.kodilla.ecommercee.mapper;
 
 import com.kodilla.ecommercee.domain.*;
 import org.springframework.stereotype.Service;
-import java.util.*;
 
 @Service
 public class CartMapper {
-
-    private ProductMapper productMapper;
-
-    public Cart mapToCart(final CartDto cartDto) {
-        return Cart.builder()
-                .id(cartDto.getId())
-                .build();
-    }
 
     public CartDto mapToCartDto(final Cart cart) {
         return CartDto.builder()
                 .id(cart.getId())
                 .build();
     }
-
 }
