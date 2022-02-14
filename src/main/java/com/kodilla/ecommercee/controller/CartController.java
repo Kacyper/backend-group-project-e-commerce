2f00d07fee6fe40d82f7466959d5d74db74c4ab4
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-14 23:48:48 +0100

    addProductToCart() refactored in CartController

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -5,7 +5,6 @@ import com.kodilla.ecommercee.exception.CartNotFoundException;
 import com.kodilla.ecommercee.exception.ProductNotFoundInCartException;
 import com.kodilla.ecommercee.mapper.*;
 import com.kodilla.ecommercee.service.DbServiceCart;
-import com.kodilla.ecommercee.service.DbServiceOrder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -24,12 +23,12 @@ public class CartController {
         return ResponseEntity.ok(ProductMapper.mapToListDto(dbServiceCart.getAllProducts(idCart)));
     }
 
-    @PutMapping("/{idCart}/{idProduct}")
+    @PutMapping("addProduct/{idCart}/{idProduct}")
     public ResponseEntity<CartDto> addProductToCart(@PathVariable Long idCart, @PathVariable Long idProduct) throws CartNotFoundException, ProductNotFoundInCartException {
         return ResponseEntity.ok(cartMapper.mapToCartDto(dbServiceCart.updateCart(idCart, idProduct)));
     }
 
-    @DeleteMapping("/{idCart}/{idProduct}")
+    @PutMapping("/{idCart}/{idProduct}")
     public ResponseEntity<CartDto> deleteProductFromCart(@PathVariable Long idCart, @PathVariable Long idProduct) throws CartNotFoundException, ProductNotFoundInCartException {
         return ResponseEntity.ok(cartMapper.mapToCartDto(dbServiceCart.deleteFromCart(idCart, idProduct)));
     }
