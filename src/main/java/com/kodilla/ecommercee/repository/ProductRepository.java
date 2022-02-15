8d6bc6316cd07f45f143eeba529e9b16a01e1866
Kacper <kksykiewicz@gmail.com>
2022-02-15 16:49:37 +0100

    Poprawki.

--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package com.kodilla.ecommercee.repository;
 import com.kodilla.ecommercee.domain.Product;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
