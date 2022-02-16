e00f1cb287e2eb81b7a088e2c1627cd879daa09d
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-16 17:52:56 +0100

    ready to pull

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -1,7 +1,6 @@
 package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.Group;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -11,6 +10,5 @@ public interface GroupRepository extends CrudRepository<Group, Long> {
     @Override
     List<Group> findAll();
 
-    @Query
     boolean existsGroupByGroupName(String groupName);
 }
