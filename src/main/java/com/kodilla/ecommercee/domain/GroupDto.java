f8e8f1c27e37c512540d44760d3120cd4e20b66f
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-16 23:29:48 +0100

    GroupDto parameter validator added

--- src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
@@ -1,11 +1,13 @@
 package com.kodilla.ecommercee.domain;
 
+import com.kodilla.ecommercee.Validator.Request.GroupDtoRequestValid;
 import lombok.*;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@GroupDtoRequestValid
 public class GroupDto {
     private Long id;
     private String groupName;
