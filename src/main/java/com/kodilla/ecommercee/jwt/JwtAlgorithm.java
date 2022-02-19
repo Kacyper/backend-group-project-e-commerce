a16e68fa51413f6e3292f7bd5522e4f1a3944ac0
Piotr Kusibab <starszykruku@op.pl>
2022-02-19 18:47:24 +0100

    fixed

--- src/main/java/com/kodilla/ecommercee/jwt/JwtAlgorithm.java
+++ src/main/java/com/kodilla/ecommercee/jwt/JwtAlgorithm.java
@@ -1,15 +1,14 @@
 package com.kodilla.ecommercee.jwt;
 
 import com.auth0.jwt.algorithms.Algorithm;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtAlgorithm {
     private final Algorithm algorithm;
 
-    public JwtAlgorithm(@Value("${jwt.secret}")String secret) {
-        this.algorithm = Algorithm.HMAC512(secret);
+    public JwtAlgorithm() {
+        this.algorithm = Algorithm.HMAC512("secret");
     }
     public Algorithm getAlgorithm() {
         return algorithm;
