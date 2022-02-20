06ffd6c1e2f14ce9f1b8d719b4015a28cf57508a
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-20 12:01:49 +0100

    merge with JDP220102-56

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -2,8 +2,11 @@ package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface GroupRepository extends CrudRepository<Group, Long> {
 
     @Override
