01efe2ea8583ed43c868c2898373d8198ed2db78
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-20 20:39:11 +0100

    literowka

--- src/main/java/com/kodilla/ecommerce/configuration/CoreConfiguration.java
+++ src/main/java/com/kodilla/ecommerce/configuration/CoreConfiguration.java
@@ -34,7 +34,7 @@ public class CoreConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.kodilla.ecommercee"))
+                .apis(RequestHandlerSelectors.basePackage("com.kodilla.ecommerce"))
                 .paths(PathSelectors.any())
                 .build()
                 .securitySchemes(singletonList(createSchema()))
