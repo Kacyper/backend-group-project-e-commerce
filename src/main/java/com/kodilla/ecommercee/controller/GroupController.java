5b2ecec99b6cb2d5a8cca54443289546e6c093f5
Piotr Kusibab <starszykruku@op.pl>
2022-02-20 13:58:57 +0100

    conflicts resolved

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -1,6 +1,5 @@
 package com.kodilla.ecommercee.controller;
 
-import com.kodilla.ecommercee.domain.Group;
 import com.kodilla.ecommercee.domain.GroupDto;
 import com.kodilla.ecommercee.exception.GroupExistInRepositoryException;
 import com.kodilla.ecommercee.exception.GroupNameIsEmptyException;
@@ -9,7 +8,6 @@ import com.kodilla.ecommercee.exception.ProductNotFoundException;
 import com.kodilla.ecommercee.mapper.GroupMapper;
 import com.kodilla.ecommercee.service.DbServiceGroup;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -37,11 +35,6 @@ public class GroupController {
     }
 
     @PreAuthorize("hasAuthority('ADMIN')")
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> createGroup(@RequestBody GroupDto groupDto) {
-        Group group = GroupMapper.mapToGroup(groupDto);
-        dbServiceGroup.saveGroup(group);
-        return ResponseEntity.ok().build();
     @PostMapping
     public ResponseEntity<GroupDto> createGroup(@RequestBody GroupDto groupDto) throws GroupExistInRepositoryException, GroupNameIsEmptyException, ProductNotFoundException {
         return ResponseEntity.ok(groupMapper.mapToGroupDto(dbServiceGroup.createGroup(groupMapper.mapToGroup(groupDto))));
@@ -52,4 +45,5 @@ public class GroupController {
     public ResponseEntity<GroupDto> updateGroup(@PathVariable Long id, @RequestParam String groupName) throws GroupNotFoundException, GroupExistInRepositoryException, GroupNameIsEmptyException {
         return ResponseEntity.ok(groupMapper.mapToGroupDto(dbServiceGroup.updateGroup(id, groupName)));
     }
+
 }
