0013bbbc1d9fd96708e3abb6fcc5f94771ccb6f3
Sylwia Krzymowska <s.krzymowska@gmail.com>
2022-02-20 01:39:17 +0100

    changes after code review

--- src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
@@ -2,9 +2,6 @@ package com.kodilla.ecommercee.mapper;
 
 import com.kodilla.ecommercee.domain.Group;
 import com.kodilla.ecommercee.domain.GroupDto;
-import com.kodilla.ecommercee.domain.Product;
-import com.kodilla.ecommercee.domain.ProductDto;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,21 +10,17 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
 public class GroupMapper {
 
     public static Group mapToGroup(final GroupDto groupDto) {
-        System.out.println("start" + " size " + groupDto.getProducts().size());
-        Group grup = Group.builder()
+        return Group.builder()
                 .id(groupDto.getId())
                 .groupName(groupDto.getGroupName())
                 .products(Optional.of(groupDto.getProducts().stream()
                                 .map(ProductMapper::mapToProduct)
                                 .collect(Collectors.toList())).
-                        orElse(new ArrayList<Product>()))
+                        orElse(new ArrayList<>()))
                 .build();
-        System.out.println("after" + " size " + groupDto.getProducts().size());
-        return grup;
     }
 
     public static GroupDto mapToGroupDto(final Group group) {
@@ -37,7 +30,7 @@ public class GroupMapper {
                 .products(Optional.of(group.getProducts().stream()
                                 .map(ProductMapper::mapToDto)
                                 .collect(Collectors.toList())).
-                        orElse(new ArrayList<ProductDto>()))
+                        orElse(new ArrayList<>()))
                 .build();
     }
 
